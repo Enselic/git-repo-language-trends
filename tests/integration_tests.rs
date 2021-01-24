3340ee71f977fbc8f658dabafe8f0314d894c877
Martin Nordholts <enselic@gmail.com>
2021-01-24 10:30:01 +0100

    Add an integration test using assert_cmd crate

---
+++ tests/integration_tests.rs
@@ -0,0 +1,19 @@
+use assert_cmd::Command;
+
+#[test]
+fn sanity_analysis_of_own_git_repo() {
+    Command::cargo_bin("git-repo-language-trends")
+        .unwrap()
+        .arg(".rs")
+        .arg(".a")
+        .arg(".")
+        .assert()
+        .success()
+        .stdout(
+            "	.rs	.a	.
+2021w04	185	4	3
+2021w03	121	0	0
+",
+        )
+        .stderr("");
+}
