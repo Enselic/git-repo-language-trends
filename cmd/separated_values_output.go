affc7c4e1b8d8f632f009639ddcceb623116c36c
Martin Nordholts <enselic@gmail.com>
2021-08-26 14:18:25 +0200

    wip

--- cmd/separated_values_output.go
+++ cmd/separated_values_output.go
@@ -10,48 +10,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
 type SeparatedValuesOutput struct {
+	args      Args
 	separator string
-	dest      io.Writer
+	dest      bufio.Writer
 }
 
-func write_header_row(writer io.Writer, separator string, columns []string) {
+func write_header_row(writer bufio.Writer, separator string, columns []string) {
 	// To get correct tab alignment, pad with spaces in place of a date
 	// on the first row
-	writer.write("          ") // for YYYY-MM-DD
+	writer.WriteString("          ") // for YYYY-MM-DD
 
 	// Now write the columns
 	for _, column := range columns {
-		writer.write(fmt.Sprintf("%s%s", separator, column))
+		writer.WriteString(fmt.Sprintf("%s%s", separator, column))
 	}
 
 	// ... and finish with a newline
-	writer.write("\n")
+	writer.WriteString("\n")
 }
 
-func write_row(writer io.Writer, separator string, columns []string, date string, column_to_lines_dict map[string]int) {
+func write_row(writer bufio.Writer, separator string, columns []string, date string, column_to_lines_dict map[string]int) {
 	// Date
-	writer.write(date)
+	writer.WriteString(date)
 
 	// Line count information
 	for _, column := range columns {
-		writer.write(fmt.Sprintf("%s%s", separator, column_to_lines_dict.get(column, 0)))
+		writer.WriteString(fmt.Sprintf("%s%s", separator, column_to_lines_dict.get(column, 0)))
 	}
 
 	// ... and finish with a newline
-	writer.write("\n")
+	writer.WriteString("\n")
 }
 
-func NewSeparatedValuesOutput(separator string) SeparatedValuesOutput {
-	return SeparatedValuesOutput{separator: separator, dest: os.Stdout}
+func NewSeparatedValuesOutput(args Args, separator string) SeparatedValuesOutput {
+	return SeparatedValuesOutput{
+		args: args,
+		 separator: separator, 
+		 dest: *bufio.NewWriter(os.Stdout)
 }
-
-//	o.dest = open(args.output, 'w')
+}
 
 func (o *SeparatedValuesOutput) start(columns []string) {
 	write_header_row(o.dest, o.separator, columns)
@@ -62,6 +65,6 @@ func (o *SeparatedValuesOutput) add_row(columns []string, date string, column_to
 }
 
 func (o *SeparatedValuesOutput) finish() {
-	print_file_written(o.args.output)
-	o.dest.close()
+	print_file_written(o.args.Output)
+	//o.dest.close() TODO: For files
 }
