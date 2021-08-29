5c08f2564876631bae5f7e89fe1a22ff1840e49e
Martin Nordholts <enselic@gmail.com>
2021-08-29 13:53:08 +0200

    wip utils

--- cmd/git-repo-language-trends/utils.go
+++ cmd/git-repo-language-trends/utils.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-// func get_extensions_sorted_by_popularity(column_to_lines_dict) {
-// r = sorted(column_to_lines_dict, key=column_to_lines_dict.get)
-// r.reverse()
-// return r
-// }
+func get_extensions_sorted_by_popularity(column_to_lines_dict map[string]int) []string {
+	var keys []string
+	for key := range column_to_lines_dict {
+		keys = append(keys, key)
+	}
+	// TODO: r = sorted(column_to_lines_dict, key=column_to_lines_dict.get)
+	// r.reverse()
+	return keys
+}
 
-// # Excludes some extensions very unlikely to be of interest, e.g. '.lock'
-// func get_top_three_extensions(column_to_lines_dict) {
-// data = get_extensions_sorted_by_popularity(column_to_lines_dict)
-// filtered_data = list(filter(lambda e: e != ".lock", data))
-// return filtered_data[0:3]
-// }
+// Excludes some extensions very unlikely to be of interest, e.g. '.lock'
+func get_top_three_extensions(column_to_lines_dict map[string]int) []string {
+	data := get_extensions_sorted_by_popularity(column_to_lines_dict)
+	// TODO: filtered_data = list(filter(lambda e: e != ".lock", data))
+	return data[0:3]
+}
 
 // func to_relative_numbers_if_enabled(args, column_to_lines) {
 // if not args.relative:
