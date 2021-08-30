fb9959460fae2ab731185df8ba94bcbe76a15f1a
Martin Nordholts <enselic@gmail.com>
2021-08-30 14:07:26 +0200

    wip

--- cmd/git-repo-language-trends/utils_test.go
+++ cmd/git-repo-language-trends/utils_test.go
@@ -7,55 +7,55 @@ import (
 )
 
 func TestGetTopThreeExtensionsFrom_0(t *testing.T) {
-    performMapToArrayTest({}, get_top_three_extensions, [])
+	performMapToArrayTest(t, map[string]int{}, get_top_three_extensions, []string{})
 }
 
 func TestGetTopThreeExtensionsFrom_1(t *testing.T) {
-    performMapToArrayTest(
-        {".rs": 10},
-        get_top_three_extensions,
-        [".rs"],
-    )
+	performMapToArrayTest(t,
+		map[string]int{".rs": 10},
+		get_top_three_extensions,
+		[]string{".rs"},
+	)
 }
 
 func TestGetTopThreeExtensionsFrom_2(t *testing.T) {
-    performMapToArrayTest(
-        {".rs": 10, ".foo": 100},
-        get_top_three_extensions,
-        [".foo", ".rs"],
-    )
+	performMapToArrayTest(t,
+		map[string]int{".rs": 10, ".foo": 100},
+		get_top_three_extensions,
+		[]string{".foo", ".rs"},
+	)
 }
 
 func TestGetTopThreeExtensionsFrom_3(t *testing.T) {
-    performMapToArrayTest(
-        {".rs": 100, ".foo": 10, ".a": 1000},
-        get_top_three_extensions,
-        [".a", ".rs", ".foo"],
-    )
+	performMapToArrayTest(t,
+		map[string]int{".rs": 100, ".foo": 10, ".a": 1000},
+		get_top_three_extensions,
+		[]string{".a", ".rs", ".foo"},
+	)
 }
 
 func TestGetTopThreeExtensionsFrom_4(t *testing.T) {
-    performMapToArrayTest(
-        {".md": 5, ".rs": 100, ".foo": 10, ".a": 1000},
-        get_top_three_extensions,
-        [".a", ".rs", ".foo"],
-    )
+	performMapToArrayTest(t,
+		map[string]int{".md": 5, ".rs": 100, ".foo": 10, ".a": 1000},
+		get_top_three_extensions,
+		[]string{".a", ".rs", ".foo"},
+	)
 }
 
 func TestGetTopThreeExtensionsButLockExtIsIgnored(t *testing.T) {
-    performMapToArrayTest(
-        {".md": 5, ".rs": 100, ".foo": 10, ".lock": 1000},
-        get_top_three_extensions,
-        [".rs", ".foo", ".md"],
-    )
+	performMapToArrayTest(t,
+		map[string]int{".md": 5, ".rs": 100, ".foo": 10, ".lock": 1000},
+		get_top_three_extensions,
+		[]string{".rs", ".foo", ".md"},
+	)
 }
 
 func TestGetExtensionsSortedByPopularity(t *testing.T) {
-    performMapToArrayTest(
-        {".md": 5, ".rs": 100, ".foo": 10, ".a": 1000},
-        get_extensions_sorted_by_popularity,
-        [".a", ".rs", ".foo", ".md"],
-    )
+	performMapToArrayTest(t,
+		map[string]int{".md": 5, ".rs": 100, ".foo": 10, ".a": 1000},
+		get_extensions_sorted_by_popularity,
+		[]string{".a", ".rs", ".foo", ".md"},
+	)
 }
 
 // Helper function that tests that input hash map entries are transformed
@@ -63,9 +63,9 @@ func TestGetExtensionsSortedByPopularity(t *testing.T) {
 func performMapToArrayTest(
 	t *testing.T,
 	input_map_entries map[string]int,
-	 transformer func(map[string]int) []string,
-	  expected_output_entries []string,
-	  ) {
-    result = transformer(input_map_entries)
-    assert.Equals(t, result, expected_output_entries, "works")
+	transformer func(map[string]int) []string,
+	expected_output_entries []string,
+) {
+	result := transformer(input_map_entries)
+	assert.Equal(t, result, expected_output_entries, "works")
 }
